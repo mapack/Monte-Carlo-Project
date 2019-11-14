b7786642c4f63f150e9599bee8d51fa676cb764d
mapack <55842580+mapack@users.noreply.github.com>
2019-11-14 09:33:16 -0500

    Update Cloud_Plotting.py

--- Cloud_Plotting.py
+++ Cloud_Plotting.py
@@ -2,15 +2,24 @@ from Cloud import Cloud
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+from itertools import product, combinations
 
 D = 2.6
 N = 5
-L = 100
+L = 64
+C = 64
 
-cloud = Cloud(D,N,L).point_array
+cloudR = Cloud(D,N,L,C).point_array
+cloud = cloudR[0]
+print('Density Array: ' + str(cloudR[1][0]))
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-ax.scatter(cloud[:,0], cloud[:,1], cloud[:,2], c='r', marker='o')
+ax.scatter(cloud[:,0], cloud[:,1], cloud[:,2], c='r', marker='.')
+r = [0, L]
+for s, e in combinations(np.array(list(product(r, r, r))), 2):
+    if np.sum(np.abs(s-e)) == r[1]-r[0]:
+        ax.plot3D(*zip(s, e), color="b")
+
 plt.show()
