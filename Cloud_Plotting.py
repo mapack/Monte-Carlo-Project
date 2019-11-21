d37102c36ddbd6ccb4eea6d1c52da83e419fe697
mapack <55842580+mapack@users.noreply.github.com>
2019-11-21 13:26:14 -0500

    Update Cloud_Plotting.py

--- Cloud_Plotting.py
+++ Cloud_Plotting.py
@@ -1,31 +1,73 @@
+import argparse                  # allows us to deal with arguments to main()
+from argparse import RawTextHelpFormatter
 from Cloud import Cloud
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from itertools import product, combinations
+from MonteCarloScheme import monteCarlo
 
-D = 2.6
-N = 5
-L = 64
-C = 64
+def main():
+    parser = argparse.ArgumentParser(formatter_class=RawTextHelpFormatter)
+    parser.add_argument("D",type=float,
+                        help="Fractal Dimention")
+    parser.add_argument("L",type=float,
+                        help="Size of cube axis")
+    parser.add_argument("C",type=float,
+                        help="Number of Cubes Axis")
+    parser.add_argument("N",type=float,
+                        help="Generation particle number")
+    parser.add_argument("mat",type=str,
+                        help="Material of Cloud")
+    parser.add_argument("lmbd",type=float,
+                        help="Wavelength")
+    
+    args = parser.parse_args()
+    D = args.D
+    L = args.L
+    C = args.C
+    N = args.N
+    mat = args.mat
+    lmbd = args.lmbd 
 
-cloudR = Cloud(D,N,L,C).point_array
-cloud = cloudR[0]
-print('Density Array: ' + str(cloudR[1][0]))
+    cloudR = Cloud(D,N,L,C).point_array
+    cloud = cloudR[0]
+    density = cloudR[1][0]
+#    print('Density Array: ' + str(cloudR[1][0]))
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+    #Plotting For the Particles in Space
+     
+#    fig = plt.figure()
+#    ax = fig.add_subplot(111, projection='3d')
+#
+#    ax.scatter(cloud[:,0], cloud[:,1], cloud[:,2], c='r', marker='.')
+#    r = [0, L]
+#    for s, e in combinations(np.array(list(product(r, r, r))), 2):
+#        if np.sum(np.abs(s-e)) == r[1]-r[0]:
+#            ax.plot3D(*zip(s, e), color="b")
+#        
+#    u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+#    x = L/2*np.cos(u)*np.sin(v) + L/2
+#    y = L/2*np.sin(u)*np.sin(v) + L/2
+#    z = L/2*np.cos(v) + L/2
+#    ax.plot_wireframe(x, y, z, color="gray",alpha = 0.5)
+#
+#    plt.show()
 
-ax.scatter(cloud[:,0], cloud[:,1], cloud[:,2], c='r', marker='.')
-r = [0, L]
-for s, e in combinations(np.array(list(product(r, r, r))), 2):
-    if np.sum(np.abs(s-e)) == r[1]-r[0]:
-        ax.plot3D(*zip(s, e), color="b")
+    theta, phi = np.mgrid[0:np.pi:90j, 0:2*np.pi:90j]
+    Kobs = np.zeros([8100,2])
+    for i in range(theta.size):
+        for j in range(phi.size):
+            Kobs[90*i+j,0] = theta[i] 
+            Kobs[90*i+j,1] = phi[90*i+j]
+    
+    R = L/2
+    if mat == 'test':
+        Aobs = np.zeros([20,3]) + R
+        for n in range(20):
+            Aobs[i,2] += i/(49.342*0.019)
+    else:
+        Aobs = np.zeros([10,000,3])
         
-#u, v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
-#x = L/2*np.cos(u)*np.sin(v) + L/2
-#y = L/2*np.sin(u)*np.sin(v) + L/2
-#z = L/2*np.cos(v) + L/2
-#ax.plot_wireframe(x, y, z, color="gray",alpha = 0.5)
-
-plt.show()
+    
+    intensity = monteCarlo(density,mat,lmbd,Aobs,Kobs,10,1e-2)
